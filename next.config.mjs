0e2b738dd4182f5415ca9bf3c853ecd8ad6d43c2
bahoma31-jpg <kaderamb1@gmail.com>
2026-02-24 22:09:04 +0100

    chore: remove legacy next.config.mjs to avoid config conflict

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Future: Configure webpack for Monaco Editor
-  // webpack: (config) => { ... }
-};
-
-export default nextConfig;
