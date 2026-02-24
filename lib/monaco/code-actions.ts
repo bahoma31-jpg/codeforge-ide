deb18c2c3a6944f84685a5858bfd80d151b56578
bahoma31-jpg <kaderamb1@gmail.com>
2026-02-24 21:49:17 +0100

    feat(monaco): add code actions and quick fixes

---
+++ lib/monaco/code-actions.ts
@@ -0,0 +1,37 @@
+﻿import * as monaco from "monaco-editor";
+
+export function registerCodeActions() {
+  monaco.languages.registerCodeActionProvider("typescript", {
+    provideCodeActions: (model, range, context) => {
+      const actions: monaco.languages.CodeAction[] = [];
+
+      if (context.only === "quickfix") {
+        const word = model.getWordAtPosition(range.getStartPosition());
+        if (word) {
+          actions.push({
+            title: `Log "${word.word}" to console`,
+            kind: "quickfix",
+            edit: {
+              edits: [
+                {
+                  resource: model.uri,
+                  textEdit: {
+                    range: {
+                      startLineNumber: range.endLineNumber,
+                      startColumn: 1,
+                      endLineNumber: range.endLineNumber,
+                      endColumn: 1,
+                    },
+                    text: `console.log("${word.word}", ${word.word});\n`,
+                  },
+                },
+              ],
+            },
+          });
+        }
+      }
+
+      return { actions, dispose: () => {} };
+    },
+  });
+}
